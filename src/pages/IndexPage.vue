573c769e86a298491e305cb2fe1bfc7288a68d09
Max Weidemann <max@millionways.me>
2022-09-17 12:40:57 +0200

    add image folder

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -48,7 +48,7 @@
       </div>
     </q-header>
     <div class="row justify-center items-center q-gutter-x-lg">
-      <q-img style="width: 100px; height: 100px" src="profile.png" />
+      <q-img style="width: 100px; height: 100px" src="images/profile.png" />
       <div :class="headlineClass" :style="'color: ' + invertedBackgroundColor">
         Hi, I'm Max.<br />I make apps that do stuff.
       </div>
